f372f4e94a10d8b72a6aceef26a20d745482627d
byrdie <roytsmart@gmail.com>
2017-12-23 23:56:30 -0700

    added twitter scraper to find McAfee recommendations

--- src/coin/twitter.py
+++ src/coin/twitter.py
@@ -1,12 +1,40 @@
+import string
 from twitterscraper import query_tweets
 
 query = 'from%3Aofficialmcafee'
 
 tweet_lst = query_tweets(query,10)
 
+# punctuation = set(string.punctuation)
+
 for tweet in tweet_lst:
     txt = tweet.text
     if 'Coin of the day' in txt:
-        print(txt)
-        print()
 
+        coin= ''
+        ind = []
+        j = 0
+        for i in range(len(txt)):
+
+            if txt[i].isupper():
+
+                if ind == []:
+                    ind.append(i)
+                    # print(i)
+                    # print(txt[i])
+                    j = j + 1
+                elif ind[j - 1] == i - 1:
+                    ind.append(i)
+                    # print(i)
+                    j = j + 1
+            else:
+                if j >= 2:
+                    break
+                else:
+                    ind = []
+                    j = 0
+
+        for i in ind:
+            coin = coin + txt[i]
+
+        print(coin)
